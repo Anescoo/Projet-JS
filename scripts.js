5a8ae9c87f58417b7cd4b8d343df0d4104f86afb
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2021-04-01 08:19:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,28 +13,38 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json', true);
 request.onload = function () {
 
-  // On commence à acceder aux données JSON 
+  // On commence à accèder aux données JSON 
+  
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach(heroes => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
-
+     
+      // Heroes Name
       const h1 = document.createElement('h1');
       h1.textContent = heroes.name;
 
+      //Full Name 
       const p = document.createElement('p');
       heroes.biography.fullName = heroes.biography.fullName.substring(0, 300);
       p.textContent = `${heroes.biography.fullName}...`;
-
+      
+      // Place of birth 
       const m = document.createElement('p')
       heroes.biography.placeOfBirth = heroes.biography.placeOfBirth.substring(0, 300);
       m.textContent = `${heroes.biography.placeOfBirth}...`;
+     
+      // Gender 
+      const n = document.createElement('p')
+      heroes.appearance.gender = heroes.appearance.gender.substring(0, 300);
+      n.textContent = `${heroes.appearance.gender}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
       card.appendChild(m);
+      card.appendChild(n);
     });
   } else {
     const errorMessage = document.createElement('marquee');
