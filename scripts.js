7230efbc4bd9493b22850269037d9be8f41906f6
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2021-04-05 16:56:06 +0200

    CamelCase

--- scripts.js
+++ scripts.js
@@ -47,65 +47,65 @@ request.onload = function () {
       const card = document.createElement('table');
       card.setAttribute('class', 'card');
      // Image hero
-      const im = document.createElement('img')
-      im.src = heroes.images.xs
+      const img = document.createElement('img')
+      img.src = heroes.images.xs
       // Heroes Name
-      const h1 = document.createElement('thead');
-      h1.textContent = heroes.name;
+      const HeroTitle = document.createElement('thead');
+      HeroTitle.textContent = heroes.name;
 
       //Full Name 
-      const p = document.createElement('td');
+      const Fname = document.createElement('td');
       heroes.biography.fullName = heroes.biography.fullName;
-      p.textContent = 'Full Name : ' + `${heroes.biography.fullName}`;
+      Fname.textContent = 'Full Name : ' + `${heroes.biography.fullName}`;
       
       // Place of birth 
-      const m = document.createElement('td')
+      const Place = document.createElement('td')
       heroes.biography.placeOfBirth = heroes.biography.placeOfBirth;
-      m.textContent = 'Place of birth : '+ `${heroes.biography.placeOfBirth}`;
+      Place.textContent = 'Place of birth : '+ `${heroes.biography.placeOfBirth}`;
      
       // Gender 
-      const n = document.createElement('td')
+      const Gender = document.createElement('td')
       heroes.appearance.gender = heroes.appearance.gender;
-      n.textContent = 'Gender : '+`${heroes.appearance.gender}`;
+      Gender.textContent = 'Gender : '+`${heroes.appearance.gender}`;
       
       // Power Stats 
-      const b = document.createElement('td')
-      Object.entries(heroes.powerstats).forEach(([key, value]) => b.textContent+=' '+(`${key}: ${value}`));
+      const Power = document.createElement('td')
+      Object.entries(heroes.powerstats).forEach(([key, value]) => Power.textContent+=' '+(`${key}: ${value}`));
       ///for (const property in heroes.powerstats){
       // heroes.powerstats = heroes.powerstats;
       //b.textContent = `${Object.entries(heroes.powerstats)}...`;
       ///b.textContent = `${heroes.powerstats[property]}`;
     
     // Race
-    const r = document.createElement('td')
+    const Race = document.createElement('td')
     heroes.appearance.race = heroes.appearance.race;
-    r.textContent = 'Race : ' + `${heroes.appearance.race}`
+    Race.textContent = 'Race : ' + `${heroes.appearance.race}`
     // Alignement 
-    const a = document.createElement('td')
+    const Align = document.createElement('td')
     heroes.biography.alignment = heroes.biography.alignment;
-    a.textContent = 'Alignement : '+ `${heroes.biography.alignment}`;
+    Align.textContent = 'Alignement : '+ `${heroes.biography.alignment}`;
    
     // height 
-    const h = document.createElement('td')
+    const Height = document.createElement('td')
     heroes.appearance.height = heroes.appearance.height;
-    h.textContent = 'Heigth : '+ `${heroes.appearance.height}`;
+    Height.textContent = 'Heigth : '+ `${heroes.appearance.height}`;
 
      // weight 
-     const w = document.createElement('td')
+     const Weight = document.createElement('td')
      heroes.appearance.weight = heroes.appearance.weight;
-     w.textContent = 'Weigth : '+ `${heroes.appearance.weight}`;
+     Weight.textContent = 'Weigth : '+ `${heroes.appearance.weight}`;
 
       container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(im);
-      card.appendChild(p);
-      card.appendChild(m);
-      card.appendChild(n);
-      card.appendChild(b);
-      card.appendChild(r);
-      card.appendChild(a);
-      card.appendChild(h);
-      card.appendChild(w);
+      card.appendChild(HeroTitle);
+      card.appendChild(img);
+      card.appendChild(Fname);
+      card.appendChild(Place);
+      card.appendChild(Gender);
+      card.appendChild(Power);
+      card.appendChild(Race);
+      card.appendChild(Align);
+      card.appendChild(Height);
+      card.appendChild(Weight);
       
     });
   } else {
